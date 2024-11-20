8fda420571ed83d6689956881cae633c68312d42
christopher vos <christophervos522@gmail.com>
2024-11-20 12:24:51 +0200

    completed tabs component

---
+++ src/pages/Home/ui/TabButton.tsx
@@ -0,0 +1,16 @@
+import { MouseEvent, ReactNode } from "react";
+import { cn } from "../../../lib/css";
+
+type TabButton = {
+  className?: string;
+  children: ReactNode;
+  isActive?: boolean
+  onClick?: (event: MouseEvent<HTMLButtonElement>) => void
+}
+
+export default function TabButton({ className = '', children, isActive = false, onClick }: TabButton) {
+
+  return (
+    <button onClick={onClick} className={`px-2 first-of-type:border-l-2 border-r-2 border-[--border-one] ${className} ${cn(isActive, "bg-[--ac-one] text-[--text-two] border-[--ac-one]", "")} duration-200 transition-all ease-linear`}>{children}</button>
+  )
+}
--- src/pages/Home/ui/Tabs.tsx
+++ src/pages/Home/ui/Tabs.tsx
@@ -1,8 +1,30 @@
+import { useState } from "react";
+import TabButton from "./TabButton";
+import BibleView from "./BibleView";
+
+import styles from "./tabs.module.css"
+
 export default function Tabs() {
+  const [currentTab, setCurrentTab] = useState("Bible");
+
+  const handleTabClick = (text: string) => {
+    setCurrentTab(text)
+  }
+
+  const TABS = [
+    { text: "Bible", element: <BibleView /> },
+    { text: "Songs", element: null },
+    { text: "Background", element: null },
+  ]
 
   return (
-    <div className="flex w-[60%] border-r-2 border-[--border-one]">
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero, voluptas expedita facere eos est cupiditate corporis iste inventore quos sint praesentium recusandae dolor perferendis quidem saepe sit nobis delectus ut!
+    <div className={styles.body}>
+      <div className={styles.controls}>
+        {TABS.map((tab, index) => <TabButton key={index} onClick={() => handleTabClick(tab.text)} isActive={tab.text == currentTab} >{tab.text}</TabButton>)}
+      </div>
+      <div className={styles.content}>
+        {TABS.find(tabs => tabs.text == currentTab)?.element}
+      </div>
     </div>
   )
 }
