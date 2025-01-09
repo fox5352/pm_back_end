424af0496e5bfe24022c68b7c45c2d659d089c42
christopher <christophervos522@gmail.com>
2025-01-09 21:19:16 +0200

    added songs view and basic add functionality

--- src/pages/Home/ui/SongsView/SongsView.tsx
+++ src/pages/Home/ui/SongsView/SongsView.tsx
@@ -3,7 +3,8 @@ import { FileEntry, readDir } from '@tauri-apps/api/fs';
 
 import useSettings from '../../../../hooks/useSettings';
 
-import styles from './SongsView.module.css';
+import { readToSlides } from '../../../../lib/file';
+import { addSlide, Slide } from '../../../../store/playlist';
 
 function SongsView() {
   const [lyricsFiles, setLyricsFiles] = useState<FileEntry[]>([]);
@@ -32,8 +33,6 @@ function SongsView() {
           buffer.push(...entries);
         }
 
-        console.log(buffer);
-
         setLyricsFiles(buffer);
       }
     };
@@ -46,30 +45,42 @@ function SongsView() {
       <div className="border-b-2 border-[--border-one]">header</div>
       <div>
         {lyricsFiles.map((file, index) => (
-          <LyricsFile {...file} index={index} />
+          <LyricsFile {...file} index={index} key={index} />
         ))}
       </div>
     </div>
   );
 }
 
-function LyricsFile({
-  name,
-  path,
-  index,
-  children,
-}: { index: number } & FileEntry) {
+function LyricsFile({ name, path, index }: { index: number } & FileEntry) {
   const prettyName = useMemo(() => name?.split('.')[0], [name]);
+  const [slides, setSlides] = useState<Slide[]>([]);
 
-  useEffect(() => {}, [index, name]);
+  const addSlideOnDBClick = async () => {
+    if (slides.length > 0) {
+      slides.forEach((slide) => addSlide(slide));
+    }
+  };
+
+  useEffect(() => {
+    const getContentFromFile = async () => {
+      const slides: Slide[] | null = await readToSlides(path);
+
+      if (!slides) return;
+
+      setSlides(slides);
+    };
+
+    getContentFromFile();
+  }, [index, name]);
 
   return (
-    <div
-      className="flex flex-col items-start border-2 border-[--border-one]  p-0.5 px-1 text-start hover:border-[--ac-one] hover:bg-[--ac-one] hover:text-[--text-two] duration-100 transition-all ease-linear space-y-1"
-      key={index}
+    <button
+      onDoubleClick={addSlideOnDBClick}
+      className="flex flex-col items-start w-full border-2 border-[--border-one]  p-0.5 px-1 text-start hover:border-[--ac-one] hover:bg-[--ac-one] hover:text-[--text-two] duration-100 transition-all ease-linear space-y-1"
     >
       {prettyName}
-    </div>
+    </button>
   );
 }
 
